bc07db6f08aff2a1fe9dc0cab43213ec9eda826b
Siya Arora <145938796+Siyaarora2002@users.noreply.github.com>
2023-09-25 08:36:52 -0400

    Add files via upload

---
+++ CustomerMaintenance/CustomerMaintenance/frmAddCustomer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CustomerMaintenance
+{
+    public partial class frmAddCustomer : Form
+    {
+
+        private Customer customer = null;
+
+        public frmAddCustomer()
+        {
+            InitializeComponent();
+        }
+
+        public Customer GetNewCustomer()
+        {
+            using (var form = new frmAddCustomer())
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    return form.customer;
+                }
+                else return null;
+            }
+
+            void InitializeComponents()
+            {
+                // Initialize form controls, including the saveButton
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text;// Get the value from the FirstName field
+            string lastName = txtLastName.Text;// Get the value from the LastName field
+            string email = txtEmail.Text;// Get the value from the Email field
+
+            string validationErrors = "";
+            validationErrors += Validator.IsPresent(firstName, "First Name");
+            validationErrors += Validator.IsPresent(lastName, "Last Name");
+            validationErrors += Validator.IsValidEmail(email, "Email");
+
+            if (string.IsNullOrEmpty(validationErrors))
+            {
+                customer = new Customer(firstName, lastName, email);
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Validation errors:" + Environment.NewLine + validationErrors);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ CustomerMaintenance/CustomerMaintenance/frmCustomers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerMaintenance
+{
+    public partial class frmCustomers : Form
+    {
+
+        private List<Customer> customers = null;
+        public frmCustomers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCustomers_Load(object sender, EventArgs e)
+        {
+            customers = CustomerDB.GetCustomers();
+            AddCustomersToListBox();
+        }
+
+        private void AddCustomersToListBox()
+        {
+            lstCustomers.Items.Clear();
+            foreach (Customer customer in customers)
+            {
+                lstCustomers.Items.Add(customer.GetDisplayText());
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmAddCustomer frmAddCustomer = new frmAddCustomer();
+            Customer newCustomer = frmAddCustomer.GetNewCustomer();
+
+            if (newCustomer != null)
+            {
+                customers.Add(newCustomer);
+                CustomerDB.SaveCustomers(customers);
+                AddCustomersToListBox();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstCustomers.SelectedIndex != -1)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete the selected customer?",
+                    "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    int selectedIndex = lstCustomers.SelectedIndex;
+                    customers.RemoveAt(selectedIndex);
+                    CustomerDB.SaveCustomers(customers);
+                    AddCustomersToListBox();
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
